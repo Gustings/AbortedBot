fb60fabad1e13bc406ccad4c20881da7b216850d
Gustings <36085056+Gustings@users.noreply.github.com>
2018-03-09 17:19:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '?';
 
-client.on('Ready', () => {
+// Creator
+client.on('creator', () => {
     console.log('Ready to execute!');
 });
 
 client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-}
+    if (message.content === '?creator') {
+    	message.channel.send('This is the official Aborted Discord Bot! Created by Gustings');
+  	}
+});    
 
- if (message.content.startsWith(prefix + "owners")) {
-    message.channel.send('The owners of The Aborted discord are Bigseth and Andresen');
-  } else
-  if (message.content.startsWith(prefix + "website")) {
-    message.channel.send('AbortedBot Website: sites.google.com/view/abortedbot');
-  } 
-});
+// Website
+client.on('message', message => {
+    if (message.content === '?website') {
+    	message.reply('AbortedBot Website: https://sites.google.com/view/abortedbot');
+  	}
+});    
+
+// Owners
+client.on('message', message => {
+    if (message.content === '?owners') {
+    	message.reply('The owners of The Aborted discord are Bigseth and Andresen');
+  	}
+});    
 
         
 // THIS  MUST  BE  THIS  WAY
